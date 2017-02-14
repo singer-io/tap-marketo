55df4be91028d6b6e4fd423835174a214c41940e
John Miller <jmiller@rjmetrics.com>
2017-02-14 14:16:05 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-marketo',
-      version='0.1.1',
+      version='0.2.0',
       description='Singer.io tap for extracting data from the Marketo API',
       author='Stitch',
       url='https://github.com/singer-io/tap-marketo',
