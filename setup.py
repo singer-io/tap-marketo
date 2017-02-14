2715328bac7bea5584be58c76a5040dd23139187
John Miller <jmiller@rjmetrics.com>
2017-02-14 14:19:24 -0500

    Use correct version of singer

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-marketo',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_marketo'],
       install_requires=[
-          'singer-python>=0.6.0',
+          'singer-python>=0.1.0',
           'requests==2.12.4',
       ],
       entry_points='''
