7a842bccca6a65cf3c67e675e20abbd37db27a0e
Joshua Karstendick <joshua@stitchdata.com>
2017-05-15 15:53:42 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-marketo',
-      version='0.4.10',
+      version='0.4.11',
       description='Singer.io tap for extracting data from the Marketo API',
       author='Stitch',
       url='http://singer.io',
