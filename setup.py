c6658793102375983dc12d3488da20bb3b249d91
Mike DeLaurentis <miked@stitchdata.com>
2017-09-28 15:41:45 -0400

    Version 0.4.18

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-marketo',
-      version='0.4.17',
+      version='0.4.18',
       description='Singer.io tap for extracting data from the Marketo API',
       author='Stitch',
       url='http://singer.io',
