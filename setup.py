4f19bb760e0571521e05f82a6dd356f1c682d73d
Alexandra Smith <alex@stitchdata.com>
2017-10-18 11:54:09 -0400

    major version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-marketo',
-      version='1.0.1',
+      version='2.0.0',
       description='Singer.io tap for extracting data from the Marketo API',
       author='Stitch',
       url='http://singer.io',
