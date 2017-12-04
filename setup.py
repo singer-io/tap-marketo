fe1246fd5b8061037e0a799721f849c76e97dad6
John Miller <jmiller@rjmetrics.com>
2017-12-04 09:21:48 -0500

    Revert "No setup.py install/develop"
    This reverts commit fa951f7cbe6057802e30ebc92a6d655f1cc04581.

--- setup.py
+++ setup.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python
 
-import sys
 from setuptools import setup
-from setuptools.command.install import install
-from setuptools.command.develop import develop
-
-class CustomInstallCommand(install):
-    def run(self):
-        print("Don't do this. Instead do:")
-        print("\tpip install .")
-        sys.exit(1)
-
-
-class CustomDevelopCommand(develop):
-    def run(self):
-        print("Don't do this. Instead do:")
-        print("\tpip install -e .")
-        sys.exit(1)
-
 
 setup(name='tap-marketo',
       version='2.0.7',
@@ -46,8 +29,4 @@ setup(name='tap-marketo',
           ]
       },
       include_package_data=True,
-      cmdclass={
-          'install': CustomInstallCommand,
-          'develop': CustomDevelopCommand,
-      },
 )
