c08e25112a0dc32019204a9ef249d45f0a95490b
Nick McCoy <nick@stitchdata.com>
2018-03-27 15:20:13 -0400

    bumping version to 2.0.20

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-marketo',
-      version='2.0.19',
+      version='2.0.20',
       description='Singer.io tap for extracting data from the Marketo API',
       author='Stitch',
       url='http://singer.io',
