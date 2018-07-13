af7e012c1781088185b01caa39a4d5806935f6d7
Kyle Allan <kyle@stitchdata.com>
2018-07-13 11:52:02 -0400

    bump to 2.0.22

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 2.0.22
+  * Drops decimal points for fields marked as integers and logs a warning [#42](https://github.com/singer-io/tap-marketo/pull/42)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-marketo',
-      version='2.0.21',
+      version='2.0.22',
       description='Singer.io tap for extracting data from the Marketo API',
       author='Stitch',
       url='http://singer.io',
