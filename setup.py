8b000b14384e66e190e1bf7b1661c965629e4f18
Dan Mosora <dan@stitchdata.com>
2020-03-04 22:08:53 +0000

    Version 2.2.9 and changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-marketo',
-      version='2.2.8',
+      version='2.2.9',
       description='Singer.io tap for extracting data from the Marketo API',
       author='Stitch',
       url='http://singer.io',
