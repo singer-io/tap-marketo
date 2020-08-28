3fe9e403392c16c05ffe3d249e1dba1be21a4378
Jacob Baca <jbaca@stitchdata.com>
2020-08-28 19:37:24 +0000

    bump minor version for tap-marketo to 2.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-marketo',
-      version='2.2.9',
+      version='2.3.0',
       description='Singer.io tap for extracting data from the Marketo API',
       author='Stitch',
       url='http://singer.io',
