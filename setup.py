f6c3f8d776a93606fa629b21e4ad701b9f4cda35
KrisPersonal <66801357+KrisPersonal@users.noreply.github.com>
2021-11-10 10:32:06 -0800

    Bump version to 2.4.4 (#79)
    Co-authored-by: KrishnanG <kgurusamy@talend.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-marketo',
-      version='2.4.3',
+      version='2.4.4',
       description='Singer.io tap for extracting data from the Marketo API',
       author='Stitch',
       url='http://singer.io',
