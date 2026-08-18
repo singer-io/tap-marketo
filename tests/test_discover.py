b070c7cec4dc523a203d9d09e419c9f573d1ef55
rsaha-qlik <vpz@qlik.com>
2026-08-18 18:21:28 +0530

    addressed replication issues

--- tests/test_discover.py
+++ tests/test_discover.py
@@ -38,7 +38,8 @@ class TestDiscover(unittest.TestCase):
                  'metadata': {'table-key-properties': ['marketoGUID'],
                               'marketo.activity-id': 1,
                               'marketo.primary-attribute-name': 'webpage_id',
-                              'forced-replication-method': 'FULL_TABLE'}},
+                              'forced-replication-method': 'INCREMENTAL',
+                              'valid-replication-keys': 'activityDate'}},
                 {
                     "metadata" : {
                         "inclusion": "automatic"
@@ -148,8 +149,8 @@ class TestDiscover(unittest.TestCase):
         self.assertDictEqual(stream, result)
         root_result_metadata = next(m["metadata"] for m in result_metadata if m["breadcrumb"] == ())
         self.assertEqual(root_result_metadata.get("table-key-properties"), ["marketoGUID"])
-        self.assertEqual(root_result_metadata.get("forced-replication-method"), "FULL_TABLE")
-        self.assertIsNone(root_result_metadata.get("valid-replication-keys"))
+        self.assertEqual(root_result_metadata.get("forced-replication-method"), "INCREMENTAL")
+        self.assertEqual(root_result_metadata.get("valid-replication-keys"), "activityDate")
         self.assertEqual(11, len(result_metadata))
         self.assertEqual(7,automatic_count)
 
